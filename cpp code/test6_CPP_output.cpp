11aa15139e467dec683046968e7ddc5b00dcf38b
Ra-h-ul <rahulpandita25@gmail.com>
2023-10-17 22:27:18 +0530

    switch case added may fail for complex switch case

---
+++ cpp code/test6_CPP_output.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+template <typename T>
+	void _format_output(std::ostream& out, const T& str) 
+	{	
+		out << str;
+	}
+
+
+
+auto main() -> int {
+auto thisMonth = 5;
+
+auto&& _s__1 = thisMonth; // switch on thisMonth
+if (_s__1 == 1) { // case 1
+std::cout << "January" << std::endl;
+} else if (_s__1 == 2) { // case 2
+std::cout << "February" << std::endl;
+} else if (_s__1 == 3) { // case 3
+std::cout << "March" << std::endl;
+} else if (_s__1 == 4) { // case 4
+std::cout << "April" << std::endl;
+} else if (_s__1 == 5) { // case 5
+std::cout << "May" << std::endl;
+} else if (_s__1 == 6) { // case 6
+std::cout << "June" << std::endl;
+	}  
+
+return 0;
+}
+
+
